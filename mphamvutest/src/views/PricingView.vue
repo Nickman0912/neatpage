664b6db64d419dce962dcf77d468e940fbca10e2
Mphamvu <pcvnclemens@gmail.com>
2025-02-22 01:24:16 -0500

    testing

---
+++ mphamvutest/src/views/PricingView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pricing">
+    <h1>Pricing</h1>
+    <div class="pricing-content">
+      <p>Pricing information will go here.</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.pricing {
+  padding-top: 8rem;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding-left: 2rem;
+  padding-right: 2rem;
+}
+</style>
---
+++ mphamvutest/src/views/UpdatesView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="updates">
+    <h1>Updates</h1>
+    <div class="updates-content">
+      <p>Latest updates and news will go here.</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.updates {
+  padding-top: 8rem;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding-left: 2rem;
+  padding-right: 2rem;
+}
+</style>
